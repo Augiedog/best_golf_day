937720d8165fcad38ce36ead950a333fb26c42c7
Augie Bren <Augie>
2022-09-13 00:43:18 -0500

    should add

--- script.py
+++ script.py
@@ -45,3 +45,8 @@ print('to_graph values: ', to_graph.values())
 
 plt.scatter(to_graph.keys(), to_graph.values()) # sets up the graph
 plt.show() # paints the graph to your screen
+
+
+# Currently, if we were to start a search on one of the last 2 days of the month, our dates will look screwy, potentially going beyond the confines of agreed-upon datetime. How could you resolve the edge-cases that could cause these errors?
+# Try adapting our script to display a bar graph of the temperatures of the next three days
+# Sometimes the API returns unclean data, looking like: ' km / h' for the wind speed on any given day. Adapt your code to handle this event. Assume if wind speed is not posted, the wind speed will be 0.
